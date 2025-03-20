bcc0c8d03b2c2e86d661b2f97f5dc1d5ba1d244c
Cherica <riantsoacherica01@gmail.com>
2025-03-20 12:22:33 +0300

    commit modele

---
+++ src/comments/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from users.models import User
+from projects.models import Project
+
+class Comment(models.Model):
+    """
+    Commentaires sur les projets
+    """
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_approved = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f"Commentaire de {self.author.username} sur {self.project.title}"
+
