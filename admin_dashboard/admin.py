effde71eeeea67d3770ccdb06a800fe286f70481
Ghosth <ghleomyre@gmail.com>
2025-04-02 11:54:20 +0300

    add all models in admins.py

--- admin_dashboard/admin.py
+++ admin_dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(AdminLog)
+admin.site.register(SystemSetting)
+admin.site.register(Statistic)
+admin.site.register(Statistic)
+
+
--- investments/admin.py
+++ investments/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Investment, InvestmentMedia
+
+admin.site.register(Investment)
+admin.site.register(InvestmentMedia)
